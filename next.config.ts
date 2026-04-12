6edf9025d87518a6f91f3a711d811b7a3e8ec39a
MagicalSpyto <e.dust@gmx.net>
2026-04-12 16:36:50 +0200

    fix routing error

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,10 @@ import type { NextConfig } from "next";
 const repoName = "Portfolio";
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: `/Portfolio`,
+  //basePath: `/Portfolio`,
   //assetPrefix: `/${repoName}/`,
   reactStrictMode: true,
+
 };
 
 // module.exports = nextConfig;
